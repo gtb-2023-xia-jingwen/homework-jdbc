e81ffb72436869f398984e597eceba192a57ef18
Ke Wang <ke.wang@thoughtworks.com>
2021-03-17 16:17:40 +0800

    [wangke] chore: clean sonarqube issues

--- src/main/java/com/tw/DatabaseConnectionProvider.java
+++ src/main/java/com/tw/DatabaseConnectionProvider.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseConnectionProvider {
+    private DatabaseConnectionProvider() {
+    }
+
     public static Connection createConnection(ServiceConfiguration configuration) throws Exception {
         Class.forName(configuration.getDriver());
         return DriverManager.getConnection(
